d99cc9d7d9d1dbe61f57fc658c7c5ab8373d16e2
DESKTOP-FFF5TQA\busin <susmitapaudel@yahoo.com>
2023-11-19 08:19:09 -0600

    tEST

--- src/test/java/com/test/pages/FrontierFlightBookPageUsingPageFactory.java
+++ src/test/java/com/test/pages/FrontierFlightBookPageUsingPageFactory.java
@@ -1,5 +1,32 @@
 package com.test.pages;
 
-public class FrontierFlightBookPageUsingPageFactory {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
+public class FrontierFlightBookPageUsingPageFactory {
+	static WebDriver driver;
+	static String URL;
+	FlyFrontierFlightBookingpageObject flyFrontierflightBookingPageObject;
+	
+	@BeforeSuite
+	public void beforeTest() {
+		    driver = new ChromeDriver();	
+		    URL = "https://www.flyfrontier.com/";  
+	flyFrontierflightBookingPageObject=new FlyFrontierFlightBookingpageObject(driver);
+	}
+	@Test
+	public void frontierFlightBooking() {
+		driver.get(URL);
+		flyFrontierflightBookingPageObject.verifyPageTitle();
+		flyFrontierflightBookingPageObject.selectTrip();
+		flyFrontierflightBookingPageObject.origion();
+		flyFrontierflightBookingPageObject.destination();
+		flyFrontierflightBookingPageObject.departDate("11/22/23");
+		flyFrontierflightBookingPageObject.returnDate("12/9/23");
+		flyFrontierflightBookingPageObject.numberOfTravellers();
+		flyFrontierflightBookingPageObject.selectingSearchBy();
+		flyFrontierflightBookingPageObject.clickSearchBtn();
+	}
 }
